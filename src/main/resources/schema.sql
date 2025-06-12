7d56b1376a5f9eca45807cb2dacf94acfd36c10a
Jean Santos <jean.paul-95@hotmail.com>
2025-06-12 18:31:07 -0500

    feat: add data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE books (
     author VARCHAR(255),
     publication_date DATE,
     category VARCHAR(100),
-    rating INT,             -- 1 to 5
+    rating INT,
     visible BOOLEAN NOT NULL DEFAULT TRUE,
     stock INT NOT NULL DEFAULT 0
 );
